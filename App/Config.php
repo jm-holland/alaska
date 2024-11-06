be854a966ba52c2776c8879d7c573bc34d80a2b5
jmholland <jmholland@37avenue.fr>
2024-11-06 16:24:38 +0100

    Modification basepath

--- App/Config.php
+++ App/Config.php
@@ -7,14 +7,14 @@
  * @link       https://illaweb.fr
  */
 
-const BASEPATH = '/alaska/Web/';
+const BASEPATH = '/';
 
 
 const
     DB_HOST = 'localhost',
     DB_NAME = 'db_alaska',
-    DB_USER = 'admin',
-    DB_PASS = 'Mysql37)mord';
+    DB_USER = 'user',
+    DB_PASS = 'password';
 
 
 const MAX_POST = 4;
